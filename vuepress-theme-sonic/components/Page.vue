4d9a88ee836440dc3dae717d96a48a580ed17c64
陈梓聪 <chenzc@pvc123.com>
2020-10-23 10:50:04 +0800

    commit

--- vuepress-theme-sonic/components/Page.vue
+++ vuepress-theme-sonic/components/Page.vue
@@ -7,6 +7,12 @@
 
     <PageNav v-bind="{ sidebarItems }" />
 
+    <SideNav
+      key="sidebar-new"
+      :fixed="true"
+      :items="sidebarItems"
+    />
+
     <slot name="bottom" />
   </main>
 </template>
@@ -14,9 +20,10 @@
 <script>
 import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
+import SideNav from '@theme/components/SideNav.vue'
 
 export default {
-  components: { PageEdit, PageNav },
+  components: { PageEdit, PageNav, SideNav },
   props: ['sidebarItems'],
 }
 </script>
